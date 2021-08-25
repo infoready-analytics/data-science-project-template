9a314d0a10dc5f7993278dc114171f41da4e1c48
Bradley Jackson <bradley_jackson@ymail.com>
2021-08-25 11:55:51 +1000

    base template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML Project Template for quick initialisation',
+    author='ARQ Data Science Team',
+    license='',
+)
